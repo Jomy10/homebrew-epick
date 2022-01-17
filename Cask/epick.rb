00ae093c774f52d5c4ba84582bfd22314b6658c6
Jomy10 <jonas.vbs4@gmail.com>
2022-01-17 14:32:31 +0100

    update cask

--- Cask/epick.rb
+++ Cask/epick.rb
@@ -1,8 +1,8 @@
 cask "epick" do
 	version "0.6.1"
-	sha256 ""
+	sha256 "2ab5ad67c103d5b1fbd2f22ec784c9923cbc0cd742a5b5dd2007282a17d63483"
 	
-	url ""
+	url "https://github.com/Jomy10/homebrew-epick/releases/download/v0.6.1/epick.app.tar.gz"
 	name "epick"
 
 	desc "Color picker for creating harmonic color palettes that works on Linux, Windows, macOS and web."
