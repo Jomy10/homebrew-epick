1faf4d828f3d3f7382ce279d6478de2e8ec13222
Jomy10 <jonas.vbs4@gmail.com>
2022-01-17 14:28:45 +0100

    initial commit

---
+++ Cask/epick.rb
@@ -0,0 +1,12 @@
+cask "epick" do
+	version "0.6.1"
+	sha256 ""
+	
+	url ""
+	name "epick"
+
+	desc "Color picker for creating harmonic color palettes that works on Linux, Windows, macOS and web."
+	homepage "https://github.com/vv9k/epick"
+
+	app "epick.app"
+end
